db797c92c6068a13b71a4292c73596e2b6581c27
Franco Meloni <franco.meloni91@gmail.com>
2018-01-03 17:58:46 +0000

    Initial Commit

---
+++ SonarrBot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SonarrBot
+//
+//  Created by Franco Meloni on 03/01/2018.
+//  Copyright © 2018 Franco Meloni. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
